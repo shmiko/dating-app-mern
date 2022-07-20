cc3cab9d5cf24fb88ac582545fd796a397cf62de
shmiko <shmiko@gmail.com>
2022-07-21 06:38:51 +1000

    updated material ui header

--- shmik-dating-app-frontend/src/App.js
+++ shmik-dating-app-frontend/src/App.js
@@ -1,10 +1,10 @@
-//import logo from './logo.svg';
 import './App.css';
+import header from './components/header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Shmik Dating App</h1>
+      <header />
     </div>
   );
 }
